8d36c8a2740c6f31322c17d29f1af83686475b4c
Dootless <pothanathas05@gmail.com>
2025-11-07 19:34:43 +0700

    Commit 3: Adding member class and also adding the missing methods in book class

--- oop_solution/library_oop.py
+++ oop_solution/library_oop.py
@@ -7,3 +7,23 @@ class Book:
         self.author = author        
         self.available_copies = available_copies
         self.total_copies = available_copies
+
+    def borrow_book(self):
+        self.available_copies -= 1
+
+    def return_book(self):
+        self.available_copies += 1
+
+class Member:
+    def __init__(self, member_id, name, email):
+        self.member_id = member_id
+        self.name = name
+        self.email = email
+        self.borrowed_books = []
+
+
+    def borrow_book(self, book_id):
+        self.borrowed_books.append(book_id)
+
+    def return_book(self, book_id):
+        self.borrowed_books.remove(book_id)
